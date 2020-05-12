cc5c70b98d2b67cf9fb4649a2f5b679e9005e1f5
afarizap <afarizap@gmail.com>
2020-05-12 23:28:57 +0000

    fix tokens and add validations

--- monty_scratch.c
+++ monty_scratch.c
@@ -8,24 +8,29 @@
 int main(int argc, char *argv[])
 {
 	FILE *file_name;
-	char single_line[150], **tokenized;
-//	instruction_t func_dict[] = {{"push", funct_push}, {"pall", funct_pall}
-//				     {"pint", funt_pint}, (NULL, NULL)};
+	char single_line[150];
+	char *token;
+	int i = 1, t;
+	/*
+	*	instruction_t func_dict[] = {{"push", funct_push}, {"pall", funct_pall}
+	*				     {"pint", funt_pint}, (NULL, NULL)};
+	**/
 	if (argc < 2)
+		printf("USAGE: monty file\n"), exit (EXIT_FAILURE);
+	file_name =  fopen(argv[1], "r");
+	if (file_name == NULL)
 	{
-		printf("USAGE: monty file\n");
+		printf("Error: Can't open file %s\n", argv[1]);
 		exit (EXIT_FAILURE);
 	}
-	file_name =  fopen(argv[1], "r");
-	while(!feof(file_name))
+	for(;fgets(single_line, 150, file_name) != NULL; i++)
 	{
-		fgets(single_line, 150, file_name);
-		tokenized = tokenizer(single_line);
-		for (int i = 0; tokenized[0]; i++)
+		token = strtok(single_line, " \n");
+		for(t = 0; token != NULL; t++)
 		{
-			printf("%s\n", tokenized[i])
+			printf("line %i: token %i: %s\n", i, t, token);
+			token = strtok(NULL, "\n");
 		}
 	}
-	fclose(file_name);
-	return (0);
+	fclose(file_name); return (0);
 }
